a0eebc6ab81f6845866e30ecb205259e9fb74396
Wajma Niazi <w1niazi@hotmail.com>
2022-09-05 20:41:12 -0700

    adding first api/legislators, but not filtering in cid numbers

--- cid.py
+++ cid.py
@@ -3,6 +3,7 @@ import key
 import csv
 from opensecrets_api import OpenSecrets
 from pprint import pprint
+import unicodedata
 
 cid_list = []
 
@@ -11,13 +12,8 @@ with open('/Users/timmy the man/Desktop/Project VoteSmart/filtered_all_candiate.
     for row in csv_file_data:
         cid_list.append(row[0])
 
-pprint(cid_list)
-
-# for row_cid in csv_file_data:
-#    cid = row_cid[0]
-#    print(cid)
-#    cid_list.append(cid)
-
 
 o = OpenSecrets(key.API_KEY)
-# by_cid = o.get_legislators(  # need cid)
+
+for each_cid in cid_list:
+    pprint(o.get_legislators(each_cid))
